0c839a7ee9e5ffaedc11871080a4b2e3180d2f22
Mike Kobit <mkobit@gmail.com>
2017-04-08 09:40:40 -0500

    Add reproducible example

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+
+subprojects {
+  apply {
+    plugin("java-library")
+    plugin("maven-publish")
+  }
+  dependencies {
+    "api"("com.google.guava:guava:21.0")
+  }
+
+  configure<PublishingExtension> {
+    publications {
+      create<MavenPublication>("mavenJavaLibrary") {
+        from(components["java"])
+      }
+    }
+  }
+}
+
+project(":invalid-library-example") {
+  dependencies {
+    "api"("com.google.inject:guice:4.0")
+  }
+}
+
