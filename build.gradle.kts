89278b432cd3b597d62f2df58e1c286577087841
Mike Kobit <mkobit@gmail.com>
2017-04-08 12:47:25 -0500

    Use newer dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ subprojects {
   }
   dependencies {
     "api"("com.google.guava:guava:21.0")
-    implementation("com.squareup.okhttp3", "okhttp", "2.7.5")
+    implementation("com.squareup.okhttp3", "okhttp", "3.6.0")
   }
 
   configure<PublishingExtension> {
