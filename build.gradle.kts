274b107ae5ca290fcf52a442cf8e376bbc03576d
Mike Kobit <mkobit@gmail.com>
2017-04-10 07:45:38 -0500

    Switch form subproject to more minimal example of just applying dependencies after a publication is created

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,26 @@
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 
-subprojects {
-  apply {
-    plugin("java-library")
-    plugin("maven-publish")
-  }
-  dependencies {
-    "api"("com.google.guava:guava:21.0")
-    implementation("com.squareup.okhttp3", "okhttp", "3.6.0")
-  }
+apply {
+  plugin("java-library")
+  plugin("maven-publish")
+}
 
-  configure<PublishingExtension> {
-    publications {
-      create<MavenPublication>("mavenJavaLibrary") {
-        from(components["java"])
-      }
+dependencies {
+  "api"("com.google.guava:guava:21.0")
+  "api"("org.apache.commons:commons-lang:3.3.5")
+  implementation("org.apache.httpcomponents", "httpclient", "4.5.3")
+}
+
+configure<PublishingExtension> {
+  publications {
+    create<MavenPublication>("mavenJavaLibrary") {
+      from(components["java"])
     }
   }
 }
-
-project(":invalid-library-example") {
-  dependencies {
-    "api"("com.google.inject:guice:4.0")
-  }
+logger.lifecycle("Applying dependencies after publication creation")
+dependencies {
+  "api"("com.google.inject:guice:4.0")
+  implementation("com.squareup.okhttp3", "okhttp", "3.6.0")
 }
-
--- invalid-library-example/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-  "api"("org.apache.commons:commons-lang:3.3.5")
-  implementation("org.apache.httpcomponents", "httpclient", "4.5.3")
-}
